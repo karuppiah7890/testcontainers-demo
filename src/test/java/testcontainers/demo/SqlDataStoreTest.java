489653c2a5800106ef6f7dbbf73de58a1144d29a
Karuppiah Natarajan <karuppiah7890@gmail.com>
2020-12-19 21:09:37 +0530

    use testcontainers to run data store tests

--- src/test/java/testcontainers/demo/SqlDataStoreTest.java
+++ src/test/java/testcontainers/demo/SqlDataStoreTest.java
@@ -1,16 +1,30 @@
 package testcontainers.demo;
 
 import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.sql.SQLException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@Testcontainers
 class SqlDataStoreTest {
+
+    @Container
+    PostgreSQLContainer<?> postgreSQLContainer =
+            new PostgreSQLContainer<>("postgres:13-alpine")
+                    .withUsername("karuppiah")
+                    .withPassword("password")
+                    .withDatabaseName("demo");
+
     @Test
     void runDummyQuery() throws SQLException {
         SqlDataStore sqlDataStore =
-                new SqlDataStore("jdbc:postgresql://localhost/postgres", "karuppiahn", "");
+                new SqlDataStore(postgreSQLContainer.getJdbcUrl(),
+                        postgreSQLContainer.getUsername(),
+                        postgreSQLContainer.getPassword());
         int result = sqlDataStore.runDummyQuery();
         assertEquals(1, result);
     }
