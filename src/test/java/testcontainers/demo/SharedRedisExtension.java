0c5c8042c82bb4c7d52ed1ab5bf15ee2e301829b
Karuppiah Natarajan <karuppiah7890@gmail.com>
2020-12-21 10:37:25 +0530

    stop containers in after all callback

--- src/test/java/testcontainers/demo/SharedRedisExtension.java
+++ src/test/java/testcontainers/demo/SharedRedisExtension.java
@@ -1,16 +1,22 @@
 package testcontainers.demo;
 
+import org.junit.jupiter.api.extension.AfterAllCallback;
 import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.Extension;
 import org.junit.jupiter.api.extension.ExtensionContext;
 
-public class SharedRedisExtension implements Extension, BeforeAllCallback {
+public class SharedRedisExtension implements Extension, BeforeAllCallback, AfterAllCallback {
     final RedisContainer redisContainer =
             new RedisContainer("redis:5.0.3-alpine");
 
     @Override
     public void beforeAll(ExtensionContext context) {
         redisContainer.start();
+    }
+
+    @Override
+    public void afterAll(ExtensionContext context) throws Exception {
+        redisContainer.stop();
     }
 
     String getHost() {
